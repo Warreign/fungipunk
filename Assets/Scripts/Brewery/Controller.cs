c7f267b1842c3cf97ca1a6e67df25830193a62ee
Warreign <warreignn@gmail.com>
2024-03-10 12:17:04 +0100

    Add customer and mc

--- Assets/Scripts/Brewery/Controller.cs
+++ Assets/Scripts/Brewery/Controller.cs
@@ -8,28 +8,16 @@ using static Mushroom;
 public class Controller : MonoBehaviour
 {
 
-    [Serializable]
-    private struct FungiCount
-    {
-        FungiType type;
-        int count;
-    }
-
-    [Serializable]
-    private struct Potion
-    {
-        string name;
-        FungiCount[] mushrooms;
-    }
-
-    // Start is called before the first frame update
-
     [SerializeField]
-    private Potion[] potions;
+    private Customer initialCustomer;
+
+    private Customer currentCustomer;
 
     void Start()
     {
         DontDestroyOnLoad(this);
+
+        currentCustomer = initialCustomer;
     }
 
     // Update is called once per frame
